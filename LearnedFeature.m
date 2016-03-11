89a17553562380eba7fb263a583269bb1d4ef53d
the-aeon <dahiyarishabh@gmail.com>
2016-03-11 23:34:10 +0530

    11:3:11:34

--- LearnedFeature.m
+++ LearnedFeature.m
@@ -13,3 +13,16 @@ end
 mu_B=round(mu_B/p);
 mu_F=round(mu_F/p);
 
+% date Scatter calculation
+
+S_B=zeros(8,8);
+S_F=zeros(8,8);
+for i=1:p
+    S_B=S_B+(B{i,1}-mu_B)*(B{i,1}-mu_B)';
+    S_F=S_F+(F{i,1}-mu_F)*(F{i,1}-mu_F)';
+end
+
+% intra and inter class scatters
+
+S_w=S_B+S_F;
+S_b=(mu_B-mu_F)*(mu_B-mu_F)';
