b8a1e74f28d31b08ca685d73ac1a8eea6dad1c26
the-aeon <dahiyarishabh@gmail.com>
2016-03-16 03:11:23 +0530

    LBP 16:3,3:10am

--- LBPfeature.m
+++ LBPfeature.m
@@ -6,17 +6,17 @@ figure,imshow(col);
 
 winSize=3;
 winSq=winSize*winSize;
+mid=ceil(winSq/2);
 imgCols=im2col(img,[winSize,winSize]);
 len=size(imgCols,2);
-Th=0.02;
-
+Th=0.05;
+lbp=cell(1,len);
 for i=1:len
-    for j=1:winSq
-        if abs(imgCols(ceil(winSq/2),i)-imgCols(j,i))<Th && j~=ceil(winSq/2)
-            
-        else if abs(imgCols(ceil(winSq/2),i)-imgCols(j,i))>=Th && j~=ceil(winSq/2)
-        end
-    end
+    curr=imgCols(:,i);
+    x=curr(mid,1);
+    curr=abs(curr-x)>Th;
+    lbp{1,i}=uniformLBP([curr(1:mid-1);curr(mid+1:winSq)]);
+    final(1,i)=sum(lbp{1,i}(mid+1:winSq-1))/(winSq-mid);
 end
-            
-        
+lbpImage=reshape(lbp,size(img,1)-winSize+1,size(img,2)-winSize+1);
+final=reshape(final,size(img,1)-winSize+1,size(img,2)-winSize+1);
